c277c76de0bd58d06c03752da8154474892262fa
Ivan Nikishev <nkshv2@gmail.com>
2026-02-11 20:00:30 +0300

    ToCUDA transformer

--- myautoml/__init__.py
+++ myautoml/__init__.py
@@ -1,2 +1,4 @@
 from .polars_transformers import *
 from .core.fitter import TabularFitter
+from .estimators import *
+from .utils.polars_utils import maybe_stack, to_dataframe, to_lazyframe, to_series
