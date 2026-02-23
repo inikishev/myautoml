cddc56988171cfb9ff5a94b6bfec122d8d31c90b
Ivan Nikishev <nkshv2@gmail.com>
2026-02-23 22:36:12 +0300

    Added RidgeClassifierProba

--- myautoml/estimators/__init__.py
+++ myautoml/estimators/__init__.py
@@ -9,6 +9,7 @@ from .hill_climbing import (
     HillClimbingEnsembleSelector,
 )
 from .kernel_approximation import LaplaceRFF
+from .ridge_classifier import RidgeClassifierCV
 from .utility import ToCUDA
 from .weighted_ensemble import (
     GreedyWeightedEnsembleClassifier,
---
+++ myautoml/estimators/ridge_classifier.py
@@ -0,0 +1,23 @@
+import numpy as np
+from scipy.special import expit, softmax # pylint:disable=no-name-in-module
+from sklearn.linear_model import RidgeClassifier, RidgeClassifierCV
+
+
+def _predict_proba(scores, n_classes):
+    if n_classes == 2: # scores is (n_samples, )
+        pos = expit(scores)
+        return np.stack([1-pos, pos], -1)
+
+    # scores is (n_samples, n_classes)
+    return softmax(scores, -1)
+
+
+class RidgeClassifierProba(RidgeClassifier):
+    def predict_proba(self, X):
+        scores = self.decision_function(X) # returns (n_samples, n_classes) or (n_samples, ) for binary
+        return _predict_proba(scores, len(self.classes_))
+
+class RidgeClassifierCVProba(RidgeClassifierCV):
+    def predict_proba(self, X):
+        scores = self.decision_function(X) # returns (n_samples, n_classes) or (n_samples, ) for binary
+        return _predict_proba(scores, len(self.classes_))
